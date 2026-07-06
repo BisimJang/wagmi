02164d9a224f2f856e28c0c3fdaa1d7144c60a8d
Jason Bisim Jang <106347006+BisimJang@users.noreply.github.com>
2026-07-06 06:27:38 +0100

    Force CORS_ALLOW_ALL_ORIGINS to True

--- wagmi-backend/core/settings.py
+++ wagmi-backend/core/settings.py
@@ -150,7 +150,7 @@ AUTH_USER_MODEL = "authwallet.WalletUser"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-CORS_ALLOW_ALL_ORIGINS = env.bool('CORS_ALLOW_ALL_ORIGINS', default=True)
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = env.list('CORS_ALLOWED_ORIGINS', default=[
     "http://localhost:5173",  # React app
     "http://localhost:5174",
